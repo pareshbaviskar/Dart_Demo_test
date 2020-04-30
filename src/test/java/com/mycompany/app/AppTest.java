14f28af0958cf540603d14b7bf2a29610bb6090f
pareshb1 <paresh.baviskar@bitwiseglobal.com>
2020-04-30 12:21:28 +0530

    this is Test in TestAppMain

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -33,11 +33,12 @@ public class AppTest
     public void testAppMain()
     {
         App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
+        assertEquals("test!", "test");
+       // try {
+           // assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        //} catch (AssertionError e) {
+          //  fail("\"message\" is not \"Hello World!\"");
+        //}
     }
 
     @After
